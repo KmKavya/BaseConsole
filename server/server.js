6cf4407686a214af03839e8242957435e2d67544
kavyavshaiva <kavya.k@evolvussolutions.com>
2018-02-12 15:11:04 +0530

    GEt users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ var {User}=require('./user');
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
 
+var {authenticate}=require('./middleware/authenticate');
+
 mongoose.connect('mongodb://localhost:27017/Users',(err,db)=> {
   if(err){
     console.log('Failed to connect');
@@ -29,6 +31,12 @@ app.post('/users',(req,res)=> {
   }).catch((e)=> {
     res.status(400).send(e);
   });
+});
+
+
+
+app.get('/users/me',authenticate,(req,res)=> {
+  res.send(req.user);
 });
 
 app.listen('3000',()=> {
